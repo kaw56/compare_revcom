4eb416d9bd27999592e73bf566975b6a4f39a8de
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-22 09:11:34 +0100

    Psuedocode
    List of what I want to achieve with this script

---
+++ comp_contigs.pl
@@ -0,0 +1,15 @@
+#!usr/bin/perl
+# comp_contigs.pl
+use strict;
+use warnings;
+
+# take the list of contigs and the probes used to assess their expression
+
+# make a hash of the values I guess or 2 hashes, one revcom the other not
+
+# loop through the contigs, find the ones that have the same id and see if
+# they are the same
+
+# if they are the same add to an array
+
+# if not add to a different array
