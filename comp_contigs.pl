515d4fa32d25d195a9b723efc46c43ab8ecabb56
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-30 09:49:49 +0100

    Testing hash of arrays formation
    Turns out I didn't really need all of that control structure
    from the previous commit because perl is clever and will
    create a new key if one doesn't exist and make a new array to
    keep my probes in.
    
    Also added some Dumper code so that I could check that the hash
    of arrays contained what I expected as I expect it to.

--- comp_contigs.pl
+++ comp_contigs.pl
@@ -2,6 +2,7 @@
 # comp_contigs.pl
 use strict;
 use warnings;
+use Data::Dumper;
 
 die "usage: comp_contigs <file with probe list>" unless (@ARGV == 1);
 
@@ -18,49 +19,38 @@ while (my $line = <$probe_list>) {
 		next;
 	}
 	if ($line =~ m/(CUST_\d+_\w+)\src_(contig\d{5})/) {
-		if (exists $reverse_probe_for{$2}) {
-			# push to the array in the hash
-		} else {
-			# add an entry to the hash and create a new array
-			$reverse_probe_for{$2} = $1;
-		}
-
-
+		push(@{$reverse_probe_for{$2}}, $1);
+		
 	} elsif ($line =~ m/(CUST_\d+_\w+)\s(contig\d{5})/) { 
-		if (exists $forward_probe_for{$2}) {
-			# push to array in hash
-		} else {
-			# add entry to forward hash and create an array
-			$forward_probe_for{$2} = $1;			
-		}
+		push(@{$forward_probe_for{$2}}, $1);
 
 	} else { 
 		# add entry to gene hash
 		$line =~ m/(CUST_\d+_\w+)\s(\w+)/;
-		$gene_probe_for{$2} = $1;
+		push(@{$gene_probe_for{$2}}, $1);
 	} 
-
 }
 
+print Dumper(\%reverse_probe_for);
 # comparing the contigs
-my @same_probes;
-my @different_probes;
+# my @same_probes;
+# my @different_probes;
 
-foreach my $contig (keys %reverse_probe_for) {
-	# get the forward and back of contig
-	my $forward = $forward_probe_for{$contig};
-	my $reverse = $reverse_probe_for{$contig};
-	if ($forward eq $reverse ) {
-		push(@same_probes, $contig)	
-	} else {
-		push(@different_probes, $contig)
-	}
-}
+# foreach my $contig (keys %reverse_probe_for) {
+# 	# get the forward and back of contig
+# 	my $forward = $forward_probe_for{$contig};
+# 	my $reverse = $reverse_probe_for{$contig};
+# 	if ($forward eq $reverse ) {
+# 		push(@same_probes, $contig)	
+# 	} else {
+# 		push(@different_probes, $contig)
+# 	}
+# }
 
 
 
-print scalar(keys %forward_probe_for), " reverse contigs\n";
-print scalar(keys %forward_probe_for), " forward contigs\n";
-print scalar(keys %gene_probe_for), " gene contigs\n";
-print scalar(@same_probes), " are the same\n";
-print scalar(@different_probes), " are different\n";
+# print scalar(keys %forward_probe_for), " reverse contigs\n";
+# print scalar(keys %forward_probe_for), " forward contigs\n";
+# print scalar(keys %gene_probe_for), " gene contigs\n";
+# print scalar(@same_probes), " are the same\n";
+# print scalar(@different_probes), " are different\n";
